618a65dcff7cce9cfeb8665e784b7fe05d7421ed
João Manaia <53819675+joaomanaia@users.noreply.github.com>
2022-11-21 22:00:26 +0000

    Added viewport to heaf

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <title>Queres ser o meu padrinho?</title>
       <meta name="description" content="Queres ser o meu padrinho?" />
       <link rel="icon" href="/favicon.ico" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
     </>
   )
 }
